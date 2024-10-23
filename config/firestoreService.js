8df374d55cd3f7bfc4a9c31a91982e8e24666fda
Amira Boubaker <amiraboubakeresprims@gmail.com>
2024-10-23 14:26:20 +0100

    add firebase config

---
+++ config/firestoreService.js
@@ -0,0 +1,11 @@
+// firestoreService.js
+import { doc, setDoc } from 'firebase/firestore';
+import { db } from './firebaseConfig'; // Ensure you import your Firestore instance
+export async function addDocument(collectionName, documentId, data) {
+  try {
+    await setDoc(doc(db, collectionName, documentId), data);
+    console.log("Document written successfully!");
+  } catch (error) {
+    console.error("Error writing document: ", error);
+  }
+}
